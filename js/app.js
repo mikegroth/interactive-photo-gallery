ebf995625e8f77f5284c83fdd9d8d80417edb70f
Mike Groth <mjaygroth@gmail.com>
2022-03-28 23:12:15 -0500

    update searchBarInput function, update CSS

--- js/app.js
+++ js/app.js
@@ -1,32 +1,36 @@
 
-
-
-// let card = "";
-
-// for (let i = 1; i <= 9; i++) {
-//     card += `<div class="gallery"><a href="photos/0${i}.jpg" data-caption="Image caption"><img src="photos/thumbnails/0${i}.jpg" alt=""></a></div>`;
-// }
-
-// for (let i = 10; i <= 12; i++) {
-//     card += `<div class="gallery"><a href="photos/${i}.jpg" data-caption="Image caption"><img src="photos/thumbnails/${i}.jpg" alt=""></a></div>`;
-// }
-
-// document.querySelector('main').innerHTML = card;
-
 baguetteBox.run('.gallery');
 
 
 function searchBarInput() {
+    
     var search = document.getElementById('searchbar');
     search.value = search.value.toLowerCase();
-    console.log(search.value);
+
+    for ( let landscapeNumber = 1; landscapeNumber <=12; landscapeNumber++ ) {
+    
+        let caption = document.getElementById(`landscape-${landscapeNumber}`);
+        caption = caption.getAttribute('data-caption').toLowerCase();
+    
+        const includesSearch = caption.includes(`${search.value}`);
+
+        if (includesSearch) {
+            document.getElementById(`landscape-${landscapeNumber}`).style.display = "initial";
+        } else {
+            document.getElementById(`landscape-${landscapeNumber}`).style.display = "none";
+        }
+    }
+
 }
 
-   
-var input = document.getElementById('searchbar');
-input = input.addEventListener("keyup", searchBarInput);
+document.addEventListener("keyup", searchBarInput);
 
-console.log(input);
+
+
+
+
+
+
 
 
 
